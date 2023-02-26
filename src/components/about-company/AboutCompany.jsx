b294bc3585eb2ee127f40043ad3642c8429a8202
antor10655gh <creativefreelancerantor@gmail.com>
2023-02-27 03:23:45 +0600

    add some change on about company

--- src/components/about-company/AboutCompany.jsx
+++ src/components/about-company/AboutCompany.jsx
@@ -8,7 +8,6 @@ import SLayout, { SDes, SMain, STitle } from "../section-layout/SectionLayout";
 
 export default function AboutCompany() {
   return (
-<<<<<<< HEAD
       <div>
         <OurCompany />
         <OurTeam />
@@ -17,18 +16,5 @@ export default function AboutCompany() {
         <Satisfaction />
         <HireUs />
       </div>
-=======
-    <div id="about">
-      <SLayout className="">
-        <SMain>
-          <div className="relative w-full overflow-hidden">
-            <div className="mb-4 relative border-green-700 lg:border-none flex justify-center text-center">
-              <STitle>Cleaning Services That Shine</STitle>
-            </div>
-          </div>
-        </SMain>
-      </SLayout>
-    </div>
->>>>>>> 729774b77c43d7d1dd280c89048b7a6cf943b852
   );
 }
