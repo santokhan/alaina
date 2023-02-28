ba9dd5193aa88280fda33ecd35cc2a5b60b4c2d0
Santo Khan <santokhan1999@gmail.com>
2023-02-28 14:18:30 +0600

    Update FAQ

--- src/views/FAQ.jsx
+++ src/views/FAQ.jsx
@@ -3,30 +3,36 @@ import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 import SectionHeader from '../components/layouts/SectionHeader';
 import '../assets/css/faq.css';
-import SLayout from '../components/section-layout/SectionLayout';
+import SLayout, {
+  SDes,
+  STitle
+} from '../components/section-layout/SectionLayout';
 import GeneralQ from '../components/GeneralQ/GeneralQ';
 import SchedulingQ from '../components/SchedulingQ/SchedulingQ';
 import CleaningQ from '../components/CleaningQ/CleaningQ';
 import BreadCrumb from '../components/BreadCrumb';
+import Container from '../components/layouts/Container';
 
 const FAQ = () => {
   return (
     <div>
       <Header />
       <BreadCrumb></BreadCrumb>
-      <SectionHeader
-        title={'Frequently Asked Questions'}
-        description={
-          'Many of our customers have specific questions about our professional maid services. Here are just a few of the frequently asked questions we hear at Cleaning Company.'
-        }
-      />
-      <div className="my-12">
-        <SLayout>
-          <GeneralQ />
-          <SchedulingQ />
-          <CleaningQ />
-        </SLayout>
-      </div>
+      <Container className="py-8 lg:py-16">
+        <STitle>Frequently Asked Questions</STitle>
+        <SDes className="text-center">
+          Many of our customers have specific questions about our professional
+          maid services. Here are just a few of the frequently asked questions
+          we hear at Cleaning Company.
+        </SDes>
+        <div className="my-12">
+          <SLayout>
+            <GeneralQ />
+            <SchedulingQ />
+            <CleaningQ />
+          </SLayout>
+        </div>
+      </Container>
       <Footer />
     </div>
   );
